0c577e1bc9cc661397d831c100a2d3b66eb23001
raghu <raghavendra.t@betterplace.co.in>
2025-03-07 14:51:16 +0530

    firstcommit

---
+++ src/test/java/Tc01.java
@@ -0,0 +1,17 @@
+import org.testng.annotations.Test;
+
+public class Tc01 {
+
+
+    public static void main(String[] args) {
+        System.out.println("raghu");
+    }
+
+
+    @Test
+    public void raghu()
+    {
+        System.out.println("raghutestng");
+    }
+
+}
